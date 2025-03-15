e4305de3dc94bac28c97dc0df7fe68f03b237bc2
anurag-nair16 <anuragnair1607.com>
2025-03-15 19:09:35 +0530

    bug fix3

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -19,7 +19,7 @@ export default function Signup() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/signup/", {
+      const response = await fetch("http://127.0.0.1:8000/user/register/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include", // Include cookies for session auth
